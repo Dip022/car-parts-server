90c8b58305319353da33280b068e4f14f4ba7745
Dipok Roy <roydipok022@gmail.com>
2022-05-27 17:20:28 +0600

    check admin success

--- index.js
+++ index.js
@@ -168,6 +168,14 @@ async function run() {
       const result = await userCollection.deleteOne(filter);
       res.send(result);
     });
+
+    // admin check
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
   } finally {
   }
 }
